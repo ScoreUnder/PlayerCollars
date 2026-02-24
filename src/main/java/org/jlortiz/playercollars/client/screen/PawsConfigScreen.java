6740268107ddc681f6ae76887333e07991e36ee3
Score_Under <seejay.11@gmail.com>
2026-02-24 17:51:45 +0000

    Use more constants in PawsConfigScreen

--- src/main/java/org/jlortiz/playercollars/client/screen/PawsConfigScreen.java
+++ src/main/java/org/jlortiz/playercollars/client/screen/PawsConfigScreen.java
@@ -15,6 +15,10 @@ public class PawsConfigScreen<T extends ItemConvertible> extends HandledScreen<P
     private static final Identifier TEXTURE = Identifier.of(PlayerCollarsMod.MOD_ID, "textures/gui/paw_controller.png");
     private static final Identifier WIDGETS_TEXTURE = Identifier.of(PlayerCollarsMod.MOD_ID, "textures/gui/paw_controller_widgets.png");
     private static final int BUTTON_HEIGHT = 16;
+    private static final int TOP_FRAME_HEIGHT = 106;
+    private static final int TOP_FRAME_WIDTH = 160;
+    private static final int TOP_FRAME_Y = 18;
+    private static final int TOP_FRAME_X = 7;
     private TagLikeListWidget<T> listWidget;
     private boolean addingTags = false;
 
@@ -28,15 +32,17 @@ public class PawsConfigScreen<T extends ItemConvertible> extends HandledScreen<P
         backgroundHeight = 222;
         playerInventoryTitleY = backgroundHeight - 94;
         super.init();
-        listWidget = addDrawableChild(new TagLikeListWidget<>(160, 106 - BUTTON_HEIGHT, x + 7, y + 18,
+        listWidget = addDrawableChild(new TagLikeListWidget<>(TOP_FRAME_WIDTH, TOP_FRAME_HEIGHT - BUTTON_HEIGHT * 2, x + TOP_FRAME_X, y + TOP_FRAME_Y,
                 client.textRenderer.fontHeight, handler.getRegistryKey(), id -> handleButtonClick(id + PawsConfigScreenHandler.LIST_ID_OFFSET)));
         listWidget.setList(handler.listToDisplay);
-        addDrawableChild(ButtonWidget.builder(Text.translatable("gui.playercollars.paw_configurator.filter.allow_all"), (btn) -> {
-            handleButtonClick(PawsConfigScreenHandler.BTN_ALLOW_ALL_ID);
-        }).dimensions(x + 7, y + 18 + 106 - BUTTON_HEIGHT, 80, BUTTON_HEIGHT).build());
-        addDrawableChild(ButtonWidget.builder(Text.translatable("gui.playercollars.paw_configurator.filter.deny_all"), (btn) -> {
-            handleButtonClick(PawsConfigScreenHandler.BTN_DENY_ALL_ID);
-        }).dimensions(x + 87, y + 18 + 106 - BUTTON_HEIGHT, 80, BUTTON_HEIGHT).build());
+        addDrawableChild(ButtonWidget
+                .builder(Text.translatable("gui.playercollars.paw_configurator.filter.allow_all"), (btn) -> handleButtonClick(PawsConfigScreenHandler.BTN_ALLOW_ALL_ID))
+                .dimensions(x + TOP_FRAME_X, y + TOP_FRAME_Y + TOP_FRAME_HEIGHT - BUTTON_HEIGHT, TOP_FRAME_WIDTH / 2, BUTTON_HEIGHT)
+                .build());
+        addDrawableChild(ButtonWidget
+                .builder(Text.translatable("gui.playercollars.paw_configurator.filter.deny_all"), (btn) -> handleButtonClick(PawsConfigScreenHandler.BTN_DENY_ALL_ID))
+                .dimensions(x + TOP_FRAME_X + TOP_FRAME_WIDTH / 2, y + TOP_FRAME_Y + TOP_FRAME_HEIGHT - BUTTON_HEIGHT, TOP_FRAME_WIDTH / 2, BUTTON_HEIGHT)
+                .build());
     }
 
     private void handleButtonClick(int id) {
@@ -78,6 +84,6 @@ public class PawsConfigScreen<T extends ItemConvertible> extends HandledScreen<P
         int x = (width - backgroundWidth - 50) / 2;
         int y = (height - backgroundHeight) / 2;
         context.drawTexture(RenderLayer::getGuiTextured, TEXTURE, x, y, 0, 0, backgroundWidth + 50, backgroundHeight, 256, 256);
-        context.drawTexture(RenderLayer::getGuiTextured, WIDGETS_TEXTURE, x + 7, y + 108, addingTags ? 0 : 16, 0, 16, 16, 32, 16);
+        context.drawTexture(RenderLayer::getGuiTextured, WIDGETS_TEXTURE, x + TOP_FRAME_X, y + TOP_FRAME_Y + TOP_FRAME_HEIGHT - 16, addingTags ? 0 : 16, 0, 16, 16, 32, 16);
     }
 }
